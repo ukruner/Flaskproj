007d44dc2959b3527a06428b4b14507c4b3a3ad5
Urmas K <urmas.kryner@gmail.com>
2023-12-19 19:26:56 +0000

    slight changes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,21 +1,18 @@
-import requests
-import json
-
-
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
 
-    if response.status_code == 500:
+    if not response.text:
+        response.status_code = 400
         return {
         'anger': None,
         'disgust': None,
         'fear': None,
         'joy': None,
         'sadness': None,
-        'dominant_emotion': None}
+        'dominant_emotion': None}, 400
 
     elif response.status_code == 200:
         dict_response = json.loads(response.text)
@@ -41,4 +38,3 @@ def emotion_detector(text_to_analyse):
         'joy': joy_score,
         'sadness': sadness_score,
         'dominant_emotion': f'{dominant_emotion}'}
-
