b6d07c471f55fa9e293a2022ad16f7e5ec61f54f
Urmas K <urmas.kryner@gmail.com>
2023-12-19 22:36:34 +0000

    final changes made

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,24 +1,24 @@
+import json
+import requests
+
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
 
-    if not response.text:
-        response.status_code = 400
+    if response.status_code == 400:
         return {
         'anger': None,
         'disgust': None,
         'fear': None,
         'joy': None,
         'sadness': None,
-        'dominant_emotion': None}, 400
+        'dominant_emotion': None}
 
     elif response.status_code == 200:
         dict_response = json.loads(response.text)
         emotion_list = dict_response['emotionPredictions'][0]['emotion']
-    # emotion_list['dominant_emotion'] = max(emotion_list.values())
-    # return emotion_list
         anger_score = emotion_list['anger']
         disgust_score = emotion_list['disgust']
         fear_score = emotion_list['fear']
--- server.py
+++ server.py
@@ -1,23 +1,23 @@
-
+"""server.py file coordinates the routes and supports API calls"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """This decorator calls emotion_analyzer function as a response to /emotionDetector"""
     text_to_analyse = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyse)
-    return response['dominant_emotion']
-    # if response['dominant_emotion']:
-    #     msg = [f"'{k}': {v}" for k, v in response.items() if k != 'dominant_emotion']
-    #     return f"For the given statement, the system response is {', '.join(msg)}. The dominant emotion is {response['dominant_emotion']}."
-    # else:
-    #     return "Invalid text! Please try again!"
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    msg = [f"'{k}': {v}" for k, v in response.items() if k != 'dominant_emotion']
+    return (f"For the given statement, the system response is {', '.join(msg)}." +
+    "The dominant emotion is {response['dominant_emotion']}.")
 
 @app.route("/")
 def render_index_page():
+    """renders main page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
